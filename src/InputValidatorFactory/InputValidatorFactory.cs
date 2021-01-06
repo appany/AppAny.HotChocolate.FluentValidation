b1d4db6ca063ff6d2cc285d1f43930537857759d
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-01-06 20:10:03 +0300

    Add InputValidatorFactory comment

--- src/InputValidatorFactory/InputValidatorFactory.cs
+++ src/InputValidatorFactory/InputValidatorFactory.cs
@@ -2,6 +2,10 @@ using System.Collections.Generic;
 
 namespace AppAny.HotChocolate.FluentValidation
 {
+	/// <summary>
+	/// Resolves <see cref="IInputValidator"/> by specified convention
+	/// </summary>
+	/// <param name="inputValidatorFactoryContext"></param>
 	public delegate IEnumerable<IInputValidator> InputValidatorFactory(
 		InputValidatorFactoryContext inputValidatorFactoryContext);
 }
