a9b1b6bc08ddd0b43f214af479614fd5dd1d8f88
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-01-07 22:59:10 +0300

    Update InputValidatorFactory.cs

--- src/InputValidatorFactory/InputValidatorFactory.cs
+++ src/InputValidatorFactory/InputValidatorFactory.cs
@@ -5,7 +5,6 @@ namespace AppAny.HotChocolate.FluentValidation
 	/// <summary>
 	/// Resolves <see cref="InputValidator"/> by specified convention
 	/// </summary>
-	/// <param name="inputValidatorFactoryContext"></param>
 	public delegate IEnumerable<InputValidator> InputValidatorFactory(
 		InputValidatorFactoryContext inputValidatorFactoryContext);
 }
