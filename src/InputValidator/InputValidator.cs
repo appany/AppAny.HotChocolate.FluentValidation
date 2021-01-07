9483b60b03f4bec1883334779da8d678414d5def
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-01-07 22:53:35 +0300

    Move InputValidator, add comments, docs

--- src/InputValidator/InputValidator.cs
+++ src/InputValidator/InputValidator.cs
@@ -1,8 +1,12 @@
 using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using FluentValidation.Results;
 
 namespace AppAny.HotChocolate.FluentValidation
 {
+	/// <summary>
+	/// Abstracts <see cref="IValidator"/> execution
+	/// </summary>
 	public delegate Task<ValidationResult> InputValidator(object argument, CancellationToken cancellationToken);
 }
