7548ec6f1c93aa1fd0fd40ef86fe6f94ff9a6187
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-02-06 12:41:09 +0300

    Add UseDefaultErrorMapperWithDetails test

--- tests/AppAny.HotChocolate.FluentValidation.Tests/OverrideErrorMappers.cs
+++ tests/AppAny.HotChocolate.FluentValidation.Tests/OverrideErrorMappers.cs
@@ -89,6 +89,53 @@ namespace AppAny.HotChocolate.FluentValidation.Tests
 		}
 
 		[Fact]
+		public async Task Should_UseDefaultErrorMapperWithDetails()
+		{
+			var executor = await TestSetup.CreateRequestExecutor(builder =>
+				builder.AddFluentValidation(opt => opt.UseDefaultErrorMapperWithDetails())
+					.AddMutationType(new TestMutation(field => field.Argument("input",
+						arg => arg.Type<NonNullType<TestPersonInputType>>().UseFluentValidation())))
+					.Services.AddTransient<IValidator<TestPersonInput>, NotEmptyNameValidator>());
+
+			var result = Assert.IsType<QueryResult>(
+				await executor.ExecuteAsync(TestSetup.Mutations.WithEmptyName));
+
+			result.AssertNullResult();
+
+			var error = Assert.Single(result.Errors);
+
+			Assert.Equal(ValidationDefaults.Code, error.Code);
+			Assert.Equal(NotEmptyNameValidator.Message, error.Message);
+
+			Assert.Collection(error.Extensions,
+				code =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.CodeKey, code.Key);
+					Assert.Equal(ValidationDefaults.Code, code.Value);
+				},
+				validator =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.ValidatorKey, validator.Key);
+					Assert.Equal("NotEmptyValidator", validator.Value);
+				},
+				argument =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.ArgumentKey, argument.Key);
+					Assert.Equal(new NameString("input"), argument.Value);
+				},
+				property =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.PropertyKey, property.Key);
+					Assert.Equal("Name", property.Value);
+				},
+				severity =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.SeverityKey, severity.Key);
+					Assert.Equal(Severity.Error, severity.Value);
+				});
+		}
+
+		[Fact]
 		public async Task Should_UseDefaultErrorMapperWithExtendedDetails()
 		{
 			var executor = await TestSetup.CreateRequestExecutor(builder =>
