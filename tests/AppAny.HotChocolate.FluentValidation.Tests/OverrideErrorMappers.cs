f6a3c2927ff9176796ed558407ee238008e568d8
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-02-12 22:03:14 +0300

    Add field property to details error mapper

--- tests/AppAny.HotChocolate.FluentValidation.Tests/OverrideErrorMappers.cs
+++ tests/AppAny.HotChocolate.FluentValidation.Tests/OverrideErrorMappers.cs
@@ -139,6 +139,11 @@ namespace AppAny.HotChocolate.FluentValidation.Tests
 					Assert.Equal(ValidationDefaults.ExtensionKeys.ValidatorKey, validator.Key);
 					Assert.Equal("NotEmptyValidator", validator.Value);
 				},
+				field =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.FieldKey, field.Key);
+					Assert.Equal(new NameString("test"), field.Value);
+				},
 				argument =>
 				{
 					Assert.Equal(ValidationDefaults.ExtensionKeys.ArgumentKey, argument.Key);
@@ -193,6 +198,11 @@ namespace AppAny.HotChocolate.FluentValidation.Tests
 				{
 					Assert.Equal(ValidationDefaults.ExtensionKeys.ValidatorKey, validator.Key);
 					Assert.Equal("NotEmptyValidator", validator.Value);
+				},
+				field =>
+				{
+					Assert.Equal(ValidationDefaults.ExtensionKeys.FieldKey, field.Key);
+					Assert.Equal(new NameString("test"), field.Value);
 				},
 				argument =>
 				{
